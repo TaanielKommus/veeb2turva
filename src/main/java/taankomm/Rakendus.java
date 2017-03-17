c73b5b3aa0987c1bd3d45e6bdc6eea9065ca5893
Taaniel Kõmmus <TaanielKommus@users.noreply.github.com>
2017-03-17 15:38:12 +0200

    7

--- src/main/java/taankomm/Rakendus.java
+++ src/main/java/taankomm/Rakendus.java
@@ -15,10 +15,36 @@ public class Rakendus {
 
 	@RequestMapping("/tervita")
 	String tervitusfunktsioon3(String eesnimi){
+		if(eesnimi==null){
+			return "Nimi puudub";
+		}
 		if(eesnimi.length()==0){
 			return "Nime pikkus on 0";
 		}
 		return "Tere, "+eesnimi;
+	}
+
+	//Aritmeetiline keskmine komadega eraldatud t2isarvudest stringis
+	//koosta funktsioon "keskmine", mis tagastaks parameetrina antud stringis
+	@RequestMapping("/keskmine")
+	public String keskmine(String sisend){
+		if(sisend==null){
+			return "Sisend puudub";
+		}
+		if(sisend.equals("")){
+			return "Sisendi pikkus 0";
+		}
+		String[] m=sisend.split(",");
+		if(m.length==2){
+			try{
+			int a1 = Integer.parseInt(m[0]);
+			int a2 = Integer.parseInt(m[1]);
+			return String.valueOf((a1+a2)/2);
+		} catch(Exception ex) {
+			return "Sisendiks pole arv";
+		}
+		}
+		return sisend;
 	}
 
     public static void main(String[] args) {
--- src/test/java/taankomm/EsimeneTest.java
+++ src/test/java/taankomm/EsimeneTest.java
@@ -20,5 +20,19 @@ public class EsimeneTest{
     Rakendus r=new Rakendus();
     assertEquals("Tere, Juku", r.tervitusfunktsioon3("Juku"));
     assertEquals("Nime pikkus on 0", r.tervitusfunktsioon3(""));
+    assertEquals("Nimi puudub", r.tervitusfunktsioon3(null));
+  }
+
+  @Test
+  public void keskmiseTest1(){
+    Rakendus r=new Rakendus();
+    assertEquals("5", r.keskmine("5"));
+    assertEquals("6", r.keskmine("6"));
+    assertEquals("Sisend puudub", r.keskmine(null));
+    assertEquals("Sisendi pikkus 0", r.keskmine(""));
+    assertEquals("6", r.keskmine("5,7"));
+    assertEquals("7", r.keskmine("5,9"));
+    assertEquals("Sisendiks pole arv", r.keskmine("tere,5"));
+
   }
 }
