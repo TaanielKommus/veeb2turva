ebf4112de2c348930bea05752c35817b5ec162a9
Taaniel Kõmmus <TaanielKommus@users.noreply.github.com>
2017-03-17 13:46:43 +0200

    4

--- src/main/java/taankomm/TurvaKonf.java
+++ src/main/java/taankomm/TurvaKonf.java
@@ -2,7 +2,7 @@ package taankomm;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.authenctication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
